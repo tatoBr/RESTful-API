146e93073d754ec556ba9c961b75d5bf2d934da9
tatoBr <tato.l.santos@gmail.com>
2020-09-12 10:35:39 -0300

    add status code 401 to the variable httpStatusCode

--- bin/variables.js
+++ bin/variables.js
@@ -5,6 +5,7 @@ module.exports = {
         CREATED : 201,        
         ACCEPTED : 202,
         BAD_REQUEST : 400,
+        UNAUTHORIZED: 401,
         NOT_FOUND : 404,
         CONFLICT: 409,
         INTERNAL_SERVER_ERROR : 500
