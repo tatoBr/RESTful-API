568be856ed69a53ce7a5f42ff2101df3416a2b08
tatoBr <tato.l.santos@gmail.com>
2020-09-12 15:23:04 -0300

    fix auth_check to call next() function in try block

--- middlewares/authorization_check.js
+++ middlewares/authorization_check.js
@@ -2,12 +2,13 @@ const jwt = require( 'jsonwebtoken' );
 const { httpStatusCode, misc: { jwt_secret }} = require( '../bin/variables' );
 
 module.exports = ( req, res, next )=>{
-    try {
-        let token = req.body.token; //recebe o token do corpo da requisição
-        let verified = jwt.verify( token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
+    try {       
+        let verified = jwt.verify( req.body.token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
         req.verifiedData = verified; //adiciona os dados resultantes da verificação ao objeto da resposta
+        next();
     } catch (error) {
         //se a verificação falhar, retorna um erro
+        console.log( error );
         res.status( httpStatusCode.UNAUTHORIZED ).json({
             message: "Você não tem autorização para acessar esse recurso.",
             content: error
