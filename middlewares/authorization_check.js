9c058401702efb28b4fbc14f3979b7ca32d62bfa
tatoBr <tato.l.santos@gmail.com>
2020-09-12 10:36:16 -0300

    add authorization_check middleware

---
+++ middlewares/authorization_check.js
@@ -0,0 +1,16 @@
+const jwt = require( 'jsonwebtoken' );
+const { httpStatusCode, misc: { jwt_secret }} = require( '../bin/variables ');
+
+module.exports = ( req, res, next )=>{
+    try {
+        let token = req.body.token; //recebe o token do corpo da requisição
+        let verified = jwt.verify( token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
+        req.verifiedData = verified; //adiciona os dados resultantes da verificação ao objeto da resposta
+    } catch (error) {
+        //se a verificação falhar, retorna um erro
+        res.status( httpStatusCode.UNAUTHORIZED ).json({
+            message: "Você não tem autorização para acessar esse recurso.",
+            content: error
+        })
+    }
+}
