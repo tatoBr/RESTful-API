02cb828e7f571962badae15d1998a299591316a0
tatoBr <tato.l.santos@gmail.com>
2020-09-12 15:38:32 -0300

    add auth middleware in the pedidos protected routes

--- routers/pedido-router.js
+++ routers/pedido-router.js
@@ -1,14 +1,16 @@
 const express = require( 'express' );
 const Controller = require( '../controllers/pedido-controller' );
 
+const auth = require( '../middlewares/authorization_check' );
+
 const router = express.Router();
 const controller = new Controller();
 
-router.post( '/', controller.criar_pedido.bind( controller ) );
+router.post( '/', auth, controller.criar_pedido.bind( controller ) );
 router.get( '/', controller.listar_pedidos.bind( controller ) );
 router.get( '/:id', controller.buscar_pedido.bind( controller ) );
-router.patch( '/:id', controller.atualizar_pedido.bind( controller ) );
-router.delete( '/:id', controller.apagar_pedido.bind( controller ) );
+router.patch( '/:id', auth, controller.atualizar_pedido.bind( controller ) );
+router.delete( '/:id', auth, controller.apagar_pedido.bind( controller ) );
 
 module.exports = router;
 
