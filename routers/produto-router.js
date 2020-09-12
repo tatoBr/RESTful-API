16a1807fff4fe6cf30e42c9dab192ad011f6ecef
tatoBr <tato.l.santos@gmail.com>
2020-09-12 14:56:49 -0300

    add authentication middleware to protected routes

--- routers/produto-router.js
+++ routers/produto-router.js
@@ -1,11 +1,15 @@
 const express = require( 'express' );
 const multer = require('multer');
+const auth = require( '../middlewares/authorization_check');
 
 const Controller = require( '../controllers/produto-controller' );
 
 const router = express.Router()
 const controller = new Controller();
 
+/******************** Multer Setup ******************************/
+
+//define onde os arquivos serão salvos e qual nome terão
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback( null, './imagens/produtos');
@@ -18,11 +22,13 @@ const storage = multer.diskStorage({
     }
 })
 
+//define quais formatos de arquivos serão aceitos
 const fileFilter = (req, file, callback ) => {
     let isTypeOk = file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png'
     callback( null, isTypeOk );
 } 
 
+//cria uma instancia do multer
 const upload = multer({
     storage: storage,
     limits : {
@@ -30,11 +36,12 @@ const upload = multer({
     },
     fileFilter : fileFilter
 });
+/*********************************************************************/
 
-router.post( '/', upload.single('imagem'), controller.criar_produto.bind( controller ));
+router.post( '/', auth, upload.single('imagem'), controller.criar_produto.bind( controller ));
 router.get( '/', controller.listar_produtos.bind( controller ) );
 router.get( '/:id', controller.buscar_produto.bind( controller ) );
-router.patch( '/:id', controller.atualizar_produto.bind( controller )  );
-router.delete( '/:id', controller.apagar_produto.bind( controller ) );
+router.patch( '/:id', auth, controller.atualizar_produto.bind( controller )  );
+router.delete( '/:id', auth, controller.apagar_produto.bind( controller ) );
 
 module.exports = router;
