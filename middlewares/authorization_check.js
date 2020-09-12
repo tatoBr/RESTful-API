3f220ab64ba16eee412efef73b54f42f8933e180
tatoBr <tato.l.santos@gmail.com>
2020-09-12 15:37:42 -0300

    auth check retrieves jwt from headear or body

--- middlewares/authorization_check.js
+++ middlewares/authorization_check.js
@@ -2,8 +2,9 @@ const jwt = require( 'jsonwebtoken' );
 const { httpStatusCode, misc: { jwt_secret }} = require( '../bin/variables' );
 
 module.exports = ( req, res, next )=>{
-    try {       
-        let verified = jwt.verify( req.body.token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
+    try {
+        let token = req.headers.authorization || req.body.token; //resgata o token vindo na requisição         
+        let verified = jwt.verify( token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
         req.verifiedData = verified; //adiciona os dados resultantes da verificação ao objeto da resposta
         next();
     } catch (error) {
