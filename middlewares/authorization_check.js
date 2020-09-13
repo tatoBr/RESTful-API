2193c5b290e9a5c276b01a899fd25cd038aacc6d
tatoBr <tato.l.santos@gmail.com>
2020-09-13 10:06:01 -0300

    made some minor changes at authorization_check.js

--- middlewares/authorization_check.js
+++ middlewares/authorization_check.js
@@ -2,10 +2,10 @@ const jwt = require( 'jsonwebtoken' );
 const { httpStatusCode, misc: { jwt_secret }} = require( '../bin/variables' );
 
 module.exports = ( req, res, next )=>{
-    try {
+    try {        
         let token = req.headers.authorization || req.body.token; //resgata o token vindo na requisição         
         let verified = jwt.verify( token, process.env.JWT_SECRET || jwt_secret ); //verifica se o token é válido
-        req.verifiedData = verified; //adiciona os dados resultantes da verificação ao objeto da resposta
+        req.authPayload = verified; //adiciona os dados resultantes da verificação ao objeto da resposta
         next();
     } catch (error) {
         //se a verificação falhar, retorna um erro
