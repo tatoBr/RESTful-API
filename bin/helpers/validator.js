ec2810e71c6a6f6e02e0ae12cf45689d6edd9f1b
tatoBr <tato.l.santos@gmail.com>
2020-09-14 13:15:50 -0300

    update validotor.js to use joi module

--- bin/helpers/validator.js
+++ bin/helpers/validator.js
@@ -1,28 +1,25 @@
-class Validator{
+const joi = require( 'joi' );
+const { dbModels } = require( '../variables' );
 
-    constructor(){
-        //this._model = model;
-        this._errors = []
-    }
-
-    idFormatIsValid( id )
-    {
-        let regEx = /^[a-fA-F0-9]{24}$/;
-
-        if( typeof id != 'string' ){
-            this._errors.push( 'id não é do tipo string')
-            return false;
-        }
-        
-        else if( !regEx.test( id )){
-            this._errors.push( 'Formato de id Inválido' )
-            return false
-        }
-        else return true;
-        
-    }
+const validateId = ( id ) => {
+    const regexp = new RegExp( '^[a-fA-F0-9]{24}$');
+    return regexp.test( id );
 }
 
+const validateUserData = ( data ) => {
+    const schema = joi.object({
+        [ dbModels.USER.campos.id ] : joi.required(),
+        [ dbModels.USER.campos.nomeDeUsuario ] : joi.string().pattern(new RegExp('^[\\S-_.a-zA-Z0-9]{3,30}$')).required(),
+        [ dbModels.USER.campos.email ] : joi.string().email().required(),
+        [ dbModels.USER.campos.cpf ] : joi.number().required(), 
+        [ dbModels.USER.campos.senha ] : joi.string().pattern( new RegExp('^[\\S-_.a-zA-Z0-9]{6,30}$')).required(),
+        [ 'confirmar' + dbModels.USER.campos.senha ] : joi.ref( dbModels.USER.campos.senha ),
+        [ dbModels.USER.campos.nome ] : joi.string().pattern( /^[\s\w]{3,30}$/ ).min(3).max(30),
+        [ dbModels.USER.campos.sobrenome ] : joi.string().pattern( /^[\s\w]{3,60}$/ ).min(3).max(30),
+        [ dbModels.USER.campos.dataDeNascimento ] : joi.date()
+    })
 
+    return schema.validate( data );
+}
 
-module.exports = Validator;
+module.exports = { validateId, validateUserData };
