03d5494b3948cc7f32e51ac12d86ad122a034b3f
tatoBr <tato.l.santos@gmail.com>
2020-09-15 08:27:57 -0300

    implement validation to user controller

--- controllers/usuario-controller.js
+++ controllers/usuario-controller.js
@@ -1,10 +1,10 @@
 const UsuarioRepository = require('../repositories/usuario-repository');
-const Validator = require('../bin/helpers/validator');
+const { validateId, validateUserRegister, validateUserUpdate } = require('../bin/helpers/validator');
 const bcrypt = require('bcrypt');
-const jwt = require( 'jsonwebtoken' );
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const { misc: { jwt_secret }, dbModels: { USER }, httpStatusCode, dbModels } = require('../bin/variables');
-const { query } = require('express');
+
 
 /** Class representing a User Controller */
 class UsuarioController {
@@ -13,25 +13,38 @@ class UsuarioController {
      */
     constructor() {
         this._repo = new UsuarioRepository();
-        this._validator = new Validator();
     }
 
     /**
      * cria um novo usuário
-     * @param { Object } req 
-     * @param { Object } res 
+     * @param {*} req  requisição para o servidor
+     * @param {*} res  resposta do servidor
      */
     criar_usuario(req, res) {
-        //recupera os dados da requisição.
+        //recupera os campos do modelo usuario.
+        let { nomeDeUsuario, email, cpf, senha, nome, sobrenome } = USER.campos;
+
+        //recupera os dados do corpo da requisição e monta um novo documento 
         let usuario = {};
+
         usuario[USER.campos.id] = new mongoose.Types.ObjectId()
-        usuario[USER.campos.nomeDeUsuario] = req.body.apelido;
-        usuario[USER.campos.nome] = req.body.nome;
-        usuario[USER.campos.sobrenome] = req.body.sobrenome;
-        usuario[USER.campos.cpf] = req.body.cpf;
-        usuario[USER.campos.email] = req.body.email;
-        usuario[USER.campos.pedidos] = [];
+        usuario[nomeDeUsuario] = req.body.username;
+        usuario[email] = req.body.email;
+        usuario[cpf] = req.body.cpf;
+        usuario[senha] = req.body.senha;
+        usuario['confirmarsenha'] = req.body.confsenha;
+        usuario[nome] = req.body.nome;
+        usuario[sobrenome] = req.body.sobrenome;
         usuario[USER.campos.dataDeNascimento] = new Date();
+
+        //faz a validação dos dados e retorna um erro se alguma propriedade tiver valor inválido
+        const { error } = validateUserRegister(usuario);
+        if (error) {
+            let { details: [{ message }] } = error;
+            return res.status(httpStatusCode.BAD_REQUEST).json({
+                message: message
+            });
+        }
 
         //tenta criptografar a senha
         bcrypt.hash(req.body.senha, 8, (error, hash) => {
@@ -40,77 +53,34 @@ class UsuarioController {
                 console.error(error);
                 res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
                     message: error.message,
+                    content: error
                 })
             }
             //se a criptografia for bem sucedida, segue com a criação do usuário
             else {
                 //salva a hash da senha no campo 'senha' do documento
                 usuario[USER.campos.senha] = hash;
-
+                //cria uma id para o documento
+                usuario[USER.ID]
                 //envia o documento para o repositório para q esse complete a transação com o banco de dados
                 this._repo.create(usuario)
-                .then(result => { res.status(result.getStatusCode()).json(result.getResponse()); })
-                .catch(error => {
-                    res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
-                        message: error.message,
-                        content: error
+                    .then(result => { res.status(result.getStatusCode()).json(result.getResponse()); })
+                    .catch(error => {
+                        res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+                            message: error.message,
+                            content: error
+                        });
                     });
-                });
             }
         });
     }
 
-    logar_usuario(req, res, next) {
-        //recupera os dados do corpo da requisição
-        const email = req.body.email;
-        const username = req.body.username;
-        const senha = req.body.senha;
 
-        //involca o repositorio para completar a ação no banco de dados
-        //Verifica se existe no banco de dados um usuario com email ou nome de usuario correspoendente
-        this._repo.readAll({ $or: [
-            {[ USER.campos.nomeDeUsuario ]: username },
-            {[ USER.campos.email ] : email }
-        ]})
-        .then( query_result =>{
-            const authfail = () => { return res.status( httpStatusCode.BAD_REQUEST ).json({ message : "Falha na authenticação" })};
-            let users = query_result.getResponse().content;
-            
-            //se a query retornar vazia, então o usuario nao esta cadastrado. retorna erro
-            if (!Array.isArray( users ) || users.length <= 0) authfail();
-
-            //se o usuario for encontrado, checar correspondência da senha passada com a senha do banco de dados
-            else {
-                let user = users[0];
-                let payload = {
-                    id: user[ USER.campos.id ],
-                    username: user[ USER.campos.nomeDeUsuario],
-                    email: user[ USER.campos.email ]
-                }
-                let token = jwt.sign(
-                    payload,
-                    process.env.JWT_SECRET || jwt_secret,
-                    { expiresIn: "30m"})
-                bcrypt.compare( senha, user.senha, ( error, comparation_result ) => {
-                    if( error ) authfail(); //retorna erro se a comparação for mal sucedida
-                    else if( !comparation_result ) authfail(); // retorna erro se as senhas não corresponder
-                    else{     
-                        res.set( 'Authorization', token );                   
-                        res.status( httpStatusCode.OK ).json({
-                        message: "Usuario authenticado",
-                        content: formatToRead( user )                       
-                    })}// retorna ok se as senhas correspondem                     
-                })                
-            }
-        })
-        .catch(error => {
-        res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
-            message: error.message,
-            content: error
-        });
-    })
-}
-
+    /**
+     * busca a lista de usuários
+     * @param {*} req requisição para o servidor
+     * @param {*} res resposta do servidor
+     */
     listar_usuarios(req, res) {
         this._repo.readAll()
             .then(result => { res.status(result.getStatusCode()).json(result.getResponse()); })
@@ -119,13 +89,18 @@ class UsuarioController {
                     message: error.message,
                     content: error
                 });
-            })
+            });
     }
 
+    /**
+     * busca um usuário específico pela ID
+     * @param {*} req 
+     * @param {*} res 
+     */
     buscar_usuario(req, res) {
         const id = req.params.id;
 
-        if (!this._validator.idFormatIsValid(id)) {
+        if (!validateId(id)) {
             return res.status(httpStatusCode.BAD_REQUEST).json({
                 message: 'Formato de Id Inválido.',
                 content: id
@@ -142,31 +117,44 @@ class UsuarioController {
             })
     }
 
+
+    /**
+    * Atualiza os dados de um usuário específico
+    * @param {*} req 
+    * @param {*} res 
+    */
     atualizar_usuario(req, res) {
-        //verifica autenticidade da id
         const id = req.params.id;
 
-        if (!this._validator.idFormatIsValid(id)) {
+        //recupera os campos do modelo usuario.
+        let { nome, sobrenome, dataDeNascimento } = USER.campos
+
+        //recupera os dados do corpo da requisição e monta um novo documento
+        let usuarioAtualizado = {};
+        usuarioAtualizado[ nome ] = req.body.nome;
+        usuarioAtualizado[ sobrenome ] = req.body.sobreNome;
+        usuarioAtualizado[ dataDeNascimento ] = req.body.dataDeNascimento;
+        
+        //verifica autenticidade da id
+        if (!validateId( id )) {
             return res.status(httpStatusCode.BAD_REQUEST).json({
                 message: 'Formato de Id Inválido.',
                 content: id
             });
         }
 
-        //recupera os dados da requisição;
-        let filter = {}
-        for (let key in req.body) {
-            filter[key] = req.body[key];
-            if (!filter[key]) {
-                return res.status(httpStatusCode.BAD_REQUEST).json({
-                    message: `valor da propriedade ${key} é inválido.`,
-                    content: filter[key]
-                });
-            }
+        //faz a validação dos dados e retorna um erro se alguma propriedade tiver valor inválido
+        let { error } = validateUserUpdate( usuarioAtualizado );
+        if ( error ) {
+            console.log( error );
+            let { details: [{ message }] } = error;
+            return res.status(httpStatusCode.BAD_REQUEST).json({
+                message: message
+            });
         }
 
         //involca o repositorio para completar a ação no banco de dados
-        this._repo.update(id, filter)
+        this._repo.update(id, usuarioAtualizado )
             .then(result => {
                 res.status(result.getStatusCode()).json(result.getResponse());
             })
@@ -178,11 +166,16 @@ class UsuarioController {
             });
     }
 
+    /**
+     * apaga um usuário pela id
+     * @param {*} req 
+     * @param {*} res 
+     */
     deletar_usuario(req, res) {
         //verifica autenticidade da id
         const id = req.params.id;
 
-        if (!this._validator.idFormatIsValid(id)) {
+        if (!validateId(id)) {
             return res.status(httpStatusCode.BAD_REQUEST).json({
                 message: 'Formato de Id Inválido.',
                 content: id
@@ -201,8 +194,75 @@ class UsuarioController {
                 });
             })
     }
-}
 
+    /**
+     * Authentica um usuário e cria um token de acesso para o mesmo
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
+    logar_usuario(req, res, next) {
+        //recupera os dados do corpo da requisição
+        const email = req.body.email;
+        const username = req.body.username;
+        const senha = req.body.senha;
+
+        //involca o repositorio para completar a ação no banco de dados
+        //Verifica se existe no banco de dados um usuario com email ou nome de usuario correspoendente
+        this._repo.readAll({
+            $or: [
+                { [ USER.campos.nomeDeUsuario ]: username },
+                { [ USER.campos.email ]: email }
+            ]
+        })
+            .then( query_result => {
+                
+                const authfail = () => { return res.status(httpStatusCode.BAD_REQUEST).json({ message: "Falha na authenticação" }) };
+                let users = query_result.getResponse().content;
+
+                //se a query retornar vazia, então o usuario nao esta cadastrado. retorna erro
+                if (!Array.isArray( users ) || users.length <= 0) authfail();
+
+                //se o usuario for encontrado, checar correspondência da senha passada com a senha do banco de dados
+                else {
+                    let user = users[0];
+                    bcrypt.compare(senha, user.senha, (error, comparation_result) => {                        
+                        if ( error ) authfail(); //retorna erro se a comparação for mal sucedida
+
+                        else if ( !comparation_result ) authfail(); // retorna erro se as senhas não corresponder
+
+                        //se as senhas corresponderem, cria um token de acesso e evia para o usuário
+                        else {
+                            //dados que serão gravados no token de acesso
+                            let payload = {
+                                id: user[USER.campos.id],
+                                username: user[USER.campos.nomeDeUsuario],
+                                email: user[USER.campos.email]
+                            }
+
+                            //cria o token propriamente dito
+                            let token = jwt.sign( payload, process.env.JWT_SECRET || jwt_secret, { expiresIn: "30m" });
+
+                            //anexa o token no cabeçalho da resposta
+                            res.set('Authorization', token);
+
+                            //envia a resposta
+                            res.status( httpStatusCode.OK ).json({
+                                message: "Usuario authenticado",
+                                content: formatToRead(user)
+                            })
+                        }// retorna ok se as senhas correspondem                     
+                    })
+                }
+            })
+            .catch(error => {
+                res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+                    message: error.message,
+                    content: error
+                });
+            })
+    }
+}
 
 
 /*************************************** FUNÇOES AUXILIARES *****************************************************/
@@ -211,18 +271,18 @@ class UsuarioController {
 * @param { Document } unformated_doc Documento em formato 'bruto' vindo do banco de dados
 * @returns { Document } Documento formatado   
 */
-const formatToRead = function ( unformated_doc ) {
-    let userName = ( unformated_doc[ USER.campos.nomeDeUsuario ]) ? unformated_doc[ USER.campos.nomeDeUsuario ] : "Campor UserName Vázio";
-    let userId = ( unformated_doc[ USER.campos.id ]) ? unformated_doc[ USER.campos.id ] : "Campo Id Vázia"
-    let nome = ( unformated_doc[USER.campos.nome ]) ? unformated_doc[ USER.campos.nome ] : "Campo Primeiro Nome Vázio.";
-    let sobreNome = ( unformated_doc[ USER.campos.sobrenome ]) ? unformated_doc[ USER.campos.sobrenome ] : "Sobrenome Vázio.";
-    let email = ( unformated_doc[ USER.campos.email ]) ? unformated_doc[ USER.campos.email ] : "Campo email Vázio."
-    let pedidos = ( unformated_doc[ USER.campos.pedidos ]) ? unformated_doc[ USER.campos.pedidos ] : [ "Campo", "Pedidos", "Vázio."]
+const formatToRead = function (unformated_doc) {
+    let userName = (unformated_doc[USER.campos.nomeDeUsuario]) ? unformated_doc[USER.campos.nomeDeUsuario] : "Campor UserName Vázio";
+    let userId = (unformated_doc[USER.campos.id]) ? unformated_doc[USER.campos.id] : "Campo Id Vázia"
+    let nome = (unformated_doc[USER.campos.nome]) ? unformated_doc[USER.campos.nome] : "Campo Primeiro Nome Vázio.";
+    let sobreNome = (unformated_doc[USER.campos.sobrenome]) ? unformated_doc[USER.campos.sobrenome] : "Sobrenome Vázio.";
+    let email = (unformated_doc[USER.campos.email]) ? unformated_doc[USER.campos.email] : "Campo email Vázio."
+    let pedidos = (unformated_doc[USER.campos.pedidos]) ? unformated_doc[USER.campos.pedidos] : ["Campo", "Pedidos", "Vázio."]
 
-    let formated_doc  = {
+    let formated_doc = {
         userId: userId,
-        nome_de_usuario : userName,
-        nome_completo: `${ nome } ${ sobreNome }`,
+        nome_de_usuario: userName,
+        nome_completo: `${nome} ${sobreNome}`,
         email: email,
         pedidos: pedidos
     };
