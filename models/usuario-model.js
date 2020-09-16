07595555863a860a1336768fe670a9c056a1d029
tatoBr <tato.l.santos@gmail.com>
2020-09-16 15:10:40 -0300

    usuario model refatorado

--- models/usuario-model.js
+++ models/usuario-model.js
@@ -1,18 +1,16 @@
 const mongoose = require( 'mongoose' );
 const { misc: { emailRegEx }, dbModels, dbModels: { USER }} = require( '../bin/variables' );
 
-
-let schema = {};
-schema[USER.campos.id] = mongoose.Schema.Types.ObjectId;
-schema[USER.campos.nomeDeUsuario] = { type: String, required: true, unique: true };
-schema[USER.campos.nome] = { type : String, required : true };
-schema[USER.campos.sobrenome] = { type : String, required : true };
-schema[USER.campos.cpf] = { type : Number, required : true, unique: true };
-schema[USER.campos.email] = {type : String, required : true, unique: true, match: emailRegEx };
-schema[USER.campos.senha] = { type: String, required : true };
-schema[USER.campos.dataDeNascimento ] = { type : Date };
-schema[USER.campos.pedidos ] = { type:  [ mongoose.Schema.Types.ObjectId ] , ref: dbModels.PEDIDO.nome };
-
-const usuarioSchema = mongoose.Schema( schema );
+const usuarioSchema = mongoose.Schema({
+    [ USER.campos.id ]: mongoose.Schema.Types.ObjectId,
+    [ USER.campos.nomeDeUsuario ] : { type: String, required: true, unique : true },
+    [USER.campos.nome]: { type : String, required : true },
+    [USER.campos.sobrenome] : { type : String, required : true },
+    [USER.campos.cpf]: { type : Number, required : true, unique: true },
+    [USER.campos.email]: {type : String, required : true, unique: true, match: emailRegEx },
+    [USER.campos.senha]: { type: String, required : true },
+    [USER.campos.dataDeNascimento ]: { type : Date },
+    [USER.campos.pedidos ]: [{ type: mongoose.Schema.Types.ObjectId, ref: dbModels.PEDIDO.nome }]
+});
 
 module.exports = mongoose.model( USER.model_name, usuarioSchema );
